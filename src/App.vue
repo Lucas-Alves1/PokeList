569b17da9f49c569d6a7f7b9e643089c246fc5d0
lucas-alves1 <du.dudulucas@gmail.com>
2023-02-11 03:59:44 -0300

    Versão final do app

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,35 @@
 <script>
+import axios from "axios";
 
+export default {
+  name: 'app',
+  components: {},
+  data() {
+    return {
+      name: "",
+      pokemons: []
+    }
+  },
+  methods: {
+    mostraTexto() {
+      return this.name;
+    },
+    getId(pokemon) {
+      return pokemon.url.split("/")[6]
+    }
+  },
+  
+  mounted() {
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${this.mostraTexto()}?limit=3`).then((response) => {
+      this.pokemons = response.data.results;
+    })
+  },
+}
 </script>
 
 <template>
   <div class="container">
-    <div>
+    <div class="logo">
       <img 
         src="https://fontmeme.com/permalink/230210/1c172e1146ef8ed4174e7fc5db83540e.png" 
         alt="Logo do aplicativo Pokelist"
@@ -16,13 +41,30 @@
     </div>
 
     <div>
-      <input 
+    <label>
+        <input 
         type="text" 
         placeholder="Digite aqui:"
-        :aria-checked="true"
-      >
+        aria-checked="true"
+        v-model="name"
+        >
+    </label>
     </div>
+
+    <div class="card-container">
+      <div class="card">
+        <div class="card-info" v-for="pokemon in pokemons" :key="pokemon.name">
+
+          <img :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${getId(pokemon)}.png`" :alt="{name}" />
+          <h4>{{ pokemon.name }}</h4>
+        </div>
+      </div>
+    </div>
+
   </div>
+
+  
+
 </template>
 
 <style scoped>
@@ -31,7 +73,6 @@
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    height: 100vh;
     gap: 1rem;
   }
   
@@ -52,16 +93,43 @@
 
   .container input:focus{
     outline: 2px #356abc solid;
+  }  
+  .card-container {
+    margin: 2em 0;
+  }
+  .card-container .card {
+    background-color: #f8f8f8;
+    min-width: clamp(10rem, 70vw, 32rem);
+    border-radius: 4px;
+    margin: 0 0.5rem 0 0.5rem;
+    min-height: clamp(10rem, 20vh, 20rem);
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    place-items: center;
+    text-align: center;
+  }
+
+  .card-container .card h4 {
+    color: #181818;
+    font-size: 1.25rem;
+    font-weight: 700;
+    margin: 0;
+    padding-bottom: 0.5rem;
+  }
+  .card-container .card h4::first-letter {
+    text-transform: uppercase;
+  }
+  .container .logo img {
+    max-width: clamp(3rem, 80vw, 15rem);
+    margin: 0;
+    margin: 6rem 0 0 0;
+    animation: floating infinite 5s ease-in-out;
   }
   
   @media (max-width: 60em) {
-    .container img {
-      max-width: clamp(3rem, 80vw, 15rem);
-      margin: 0;
-    }
 
     .container .text-container {
-      font-size: clamp(1rem, 2vw, 2.5rem);
+      font-size: clamp(1.5rem, 2vw, 2.5rem);
       text-align: center;
       padding: 0 1rem 0 1rem;
     }
@@ -69,5 +137,28 @@
     .container input{
       max-width: clamp(5rem, 60vw, 40rem);
     }
+
+    .card-container .card{
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-direction: column;
+      text-align: center;
+    }
+    .card-container .card .card-info img{
+      min-width: clamp(10em, 20vw, 20em);
+    }
   }
+
+  @keyframes floating {
+    0% {
+        transform: translatey(0px);
+    }
+    50% {
+        transform: translatey(-20px);
+    }
+    100% {
+        transform: translatey(0px);
+    }
+}
 </style>
